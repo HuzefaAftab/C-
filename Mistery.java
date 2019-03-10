222eaa88dbcdb403d909682cc6512c8788a05162
HuzefaAftab <huzefaaftab98@gmail.com>
2019-03-09 21:00:21 -0600

    Update and rename HW1_Mistery.java to Mistery.java

--- Mistery.java
+++ Mistery.java
@@ -1,13 +1,13 @@
 import java.util.Arrays;
 
-public class HW1_Mistery {
+public class Mistery {
 
     
     public static void main(String[] args) {
         
     
    // Scanner in = new Scanner(System.in);
-    /* double a;
+   double a;
      double b;
      System.out.print("Enter a: ");
      a =  in.nextDouble();
@@ -21,18 +21,7 @@ public class HW1_Mistery {
      q2 = (a+b+diff)/2;
      System.out.printf("q1= %.2f %n" ,q1);
      System.out.printf("q2= %.2f%n",q2);
-   
-     
-   
-    
-    }
-*/
-  //  double[][] A2;
-   // int [][] A3= {{8,1},{3,3},{3,0},{2,6}};
-//What does this code print?
-int[][] M = {{1,2,3,4},{10,15,30,8}};
-System.out.println( Arrays.toString(M) );
-
+  
 }
 }
    
