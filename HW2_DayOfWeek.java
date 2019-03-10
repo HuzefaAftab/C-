0e012e03e9214d3040d17b913ee48b4e21248492
HuzefaAftab <huzefaaftab98@gmail.com>
2019-03-09 21:04:06 -0600

    Add files via upload

---
+++ HW2_DayOfWeek.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class HW2_DayOfWeek {
+
+   
+    public static void main(String[] args) {
+     printDay();
+     printDay();
+     printDay();
+     printDay();
+    System.out.printf("Bye%n");   
+    }
+    public static void printDay(){
+        Scanner kb = new Scanner(System.in);
+        double a;
+        System.out.print("Enter the day(1-7): ");
+        a = kb.nextDouble();
+        if (1 == a){
+            System.out.printf("%.0f -  Monday%n",a);
+        }
+        else if (2 == a){
+        System.out.printf("%.0f -  Tuesday%n",a);
+    }
+        else if (3 == a){
+            System.out.printf("%.0f - Wednesday%n",a);
+        }
+        else if (4 == a){
+            System.out.printf("%.0f -  Thursday%n",a);
+        }
+        else if (5 == a){
+            System.out.printf("%.0f - Friday%n",a);
+        }
+        else if (6 == a){
+            System.out.printf("%.0f - Saturday%n",a);
+        }
+        else if (7 == a){
+            System.out.printf("%.0f - Sunday%n",a);
+        }
+        else if (a < 1){
+        System.out.printf("Invalid%n");
+    }
+        else if (a > 7){
+        System.out.printf("Invalid%n");
+    }
+         
+    }
+      
+}
