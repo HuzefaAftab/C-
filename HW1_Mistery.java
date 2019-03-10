df9ca5890d42183b1d6a432996f0df445e811ceb
HuzefaAftab <huzefaaftab98@gmail.com>
2019-03-09 20:58:45 -0600

    Add files via upload

---
+++ HW1_Mistery.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+public class HW1_Mistery {
+
+    
+    public static void main(String[] args) {
+        
+    
+   // Scanner in = new Scanner(System.in);
+    /* double a;
+     double b;
+     System.out.print("Enter a: ");
+     a =  in.nextDouble();
+     System.out.print("Enter b: ");
+     b =  in.nextDouble();
+     double diff;
+     diff = Math.abs((a-b));
+     double q1;
+     double q2;
+     q1 = (a+b-diff)/2;
+     q2 = (a+b+diff)/2;
+     System.out.printf("q1= %.2f %n" ,q1);
+     System.out.printf("q2= %.2f%n",q2);
+   
+     
+   
+    
+    }
+*/
+  //  double[][] A2;
+   // int [][] A3= {{8,1},{3,3},{3,0},{2,6}};
+//What does this code print?
+int[][] M = {{1,2,3,4},{10,15,30,8}};
+System.out.println( Arrays.toString(M) );
+
+}
+}
+   
+        
+    
+   
+   
+
+   
+
