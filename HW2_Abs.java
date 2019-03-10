b797c98c63a50b2672f0a1d98b68b0ea506b46ba
HuzefaAftab <huzefaaftab98@gmail.com>
2019-03-09 21:00:54 -0600

    Add files via upload

---
+++ HW2_Abs.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class HW2_Abs {
+
+    
+    public static void main(String[] args) {
+       Scanner in = new Scanner(System.in);
+       double a;
+       System.out.print("Enter a: ");
+       a = in.nextDouble();
+       double b;
+       System.out.print("Enter b: ");
+       b = in.nextDouble();
+       double diff;
+       diff = (a - b);
+   
+       if (a < b) {
+           System.out.printf("%.2f - %.2f = %.2f %n%n", b , a, -diff);
+           
+       }
+       else 
+           System.out.printf("%.2f - %.2f = %.2f %n%n",a,b, diff);
+       
+    }
+    
+}
